e5604bdae31ba7312dbc8c6d4006abfa594679bc
ohmushi <omnes.theo@gmail.com>
2025-03-16 20:59:18 +0100

    add dependdencies for cucumber

--- src/main/java/cat/ohmushi/shared/annotations/DomainEntity.java
+++ src/main/java/cat/ohmushi/shared/annotations/DomainEntity.java
@@ -1,5 +1,12 @@
 package cat.ohmushi.shared.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface DomainEntity {
   public static interface DomainEntityT {
   }
